e7192fc4676ffb20a6c7c4a9f229a2bd7f7aa198
Sergio Sabater <sergiosabater@gmail.com>
2022-04-02 22:56:18 +0200

    Update: Implemented RetrofitInstance and API Service logic

--- app/src/main/java/com/sergiosabater/retrofit2/network/APIService.java
+++ app/src/main/java/com/sergiosabater/retrofit2/network/APIService.java
@@ -1,4 +1,15 @@
 package com.sergiosabater.retrofit2.network;
 
+import com.sergiosabater.retrofit2.model.PostModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface APIService {
+
+    @GET("posts")
+    Call<List<PostModel>> getPostsList();
+
 }
